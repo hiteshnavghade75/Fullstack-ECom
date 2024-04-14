f46297e9f51f554d3a813f65b16f806a79f5808f
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2024-04-14 09:01:30 +0530

    code modified

--- frontend/src/components/header/SearchBar.jsx
+++ frontend/src/components/header/SearchBar.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import './SearchBar.css'
+// import React from 'react'
+// import './SearchBar.css'
+
+// const SearchBar = () => {
+//     return (
+//         <div className='searchbar-container'>
+//             <input type='text' placeholder='Search for products' />
+//             <button className='search-button' type='submit' >Search</button>
+//         </div>
+//     )
+// }
+
+// export default SearchBar
+
+
+import React, { useState } from 'react';
+import './SearchBar.css';
+import useHandleSearch from '../../hooks/useHandleSearch';
 
 const SearchBar = () => {
+
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const {search} = useHandleSearch()
+
+    const handleChange = (event) => {
+        setSearchKeyword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        search(searchKeyword);
+    };
+
     return (
         <div className='searchbar-container'>
-            <input type='text' placeholder='Search for products' />
-            <button className='search-button' type='submit' >Search</button>
+            <input
+                type='text'
+                placeholder='Search for products'
+                value={searchKeyword}
+                onChange={handleChange}
+            />
+            <button className='search-button' type='submit' onClick={handleSubmit}>Search</button>
         </div>
-    )
-}
+    );
+};
 
-export default SearchBar
+export default SearchBar;
