756df5007d0db72b59dfff3046a2f2a0c6b9c1b2
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2024-04-14 18:05:51 +0530

    products cart page

---
+++ frontend/src/components/header/CartProduct.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import './CartProduct.css';
+
+const CartProduct = ({ele}) => {
+
+  const userString = localStorage.getItem('app-user');
+  const user = JSON.parse(userString);
+
+  return (
+    <div className="cart-product">
+      <div className="product-image-container">
+        <img src={`http://localhost:5000/images/${ele.productImage}`} className="product-image" alt="Product" />
+      </div>
+      <div className="product-details">
+        <div className="product-name-container">
+          <h3 className="product-name">{ele.productName}</h3>
+        </div>
+        <p className="product-description">{ele.description}</p>
+        <p className="product-price">
+          <span className="currency-symbol">₹</span>
+          {ele.price}
+        </p>
+        <div className="quantity-and-total-container">
+          <div className="total-label-value">
+            <p>Total : </p>
+            <p><span className="currency-symbol">₹</span>{ele.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartProduct;
